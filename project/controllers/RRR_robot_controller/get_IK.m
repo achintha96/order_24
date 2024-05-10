7255b0de00791873ce7b0addf63eeae66e46d1c4
achintha96 <achinthasurathrathnayake@gmail.com>
2024-05-11 02:52:43 +0530

    inverse kinematics method refurbished

--- project/controllers/RRR_robot_controller/get_IK.m
+++ project/controllers/RRR_robot_controller/get_IK.m
@@ -1,11 +1,15 @@
 function [theta1,theta2,theta3] = get_IK(X,Y,L_1,L_2,L_3)
-%calculating angles for given cartesian coordinate
-%% simplified using matlab
 XE = abs(X);
 YE = abs(Y+L_3);
 
-theta1 = 2*atan2((2*L_1*YE + (- L_1^4 + 2*L_1^2*L_2^2 + 2*L_1^2*XE^2 + 2*L_1^2*YE^2 - L_2^4 + 2*L_2^2*XE^2 + 2*L_2^2*YE^2 - XE^4 - 2*XE^2*YE^2 - YE^4)^(1/2)),(L_1^2 + 2*L_1*XE - L_2^2 + XE^2 + YE^2));
-theta2 = 2*atan2(((- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2)*(L_1^2 + 2*L_1*L_2 + L_2^2 - XE^2 - YE^2))^(1/2),(- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2));
+sigma1 = (- L_1^4 + 2*L_1^2*L_2^2 + 2*L_1^2*XE^2 + 2*L_1^2*YE^2 - L_2^4 + 2*L_2^2*XE^2 + 2*L_2^2*YE^2 - XE^4 - 2*XE^2*YE^2 - YE^4)^(1/2)
+num1 = (2*L_1*YE + sigma1)
+den1 = (L_1^2 + 2*L_1*XE - L_2^2 + XE^2 + YE^2)
+theta1 = 2*atan2(num1,den1);
+
+num2 = ((- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2)*(L_1^2 + 2*L_1*L_2 + L_2^2 - XE^2 - YE^2))^(1/2)
+den2 = (- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2)
+theta2 = 2*atan2(num2,den2);
 
 theta3 = (0.5*pi) - theta2 + theta1;
 theta1 = (0.5*pi) - theta1;
