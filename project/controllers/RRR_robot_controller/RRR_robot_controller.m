01762d966c9bf1674b1b41d53e61b7866eee4075
achintha96 <achinthasurathrathnayake@gmail.com>
2024-05-11 00:01:47 +0530

    Inverse kinematics fixed

---
+++ project/controllers/RRR_robot_controller/RRR_robot_controller.m
@@ -0,0 +1,136 @@
+
+TIME_STEP = 64;
+
+STAGE = 0
+SUBSTAGE = 0
+theta_1 = 0
+theta_2 = 0
+theta_3 = 0
+vel_1 = 0
+vel_2 = 0
+vel_3 = 0
+target_x = 0.25
+target_y = 0.15
+
+% initializing connector
+gripper = wb_robot_get_device('connector');
+wb_connector_enable_presence(gripper, TIME_STEP)
+
+% initializing motors
+motor_1 = wb_robot_get_device('link_1_motor');
+motor_2 = wb_robot_get_device('link_2_motor');
+motor_3 = wb_robot_get_device('link_3_motor');
+
+wb_motor_set_position(motor_1, inf)
+wb_motor_set_position(motor_2, inf)
+wb_motor_set_position(motor_3, inf)
+
+wb_motor_set_velocity(motor_1, vel_1);
+wb_motor_set_velocity(motor_2, vel_2);
+wb_motor_set_velocity(motor_3, vel_3);
+
+% initializing encoders
+encoder_1 = wb_robot_get_device('link_1_encoder')
+encoder_2 = wb_robot_get_device('link_2_encoder')
+encoder_3 = wb_robot_get_device('link_3_encoder')
+
+wb_position_sensor_enable(encoder_1, TIME_STEP)
+wb_position_sensor_enable(encoder_2, TIME_STEP)
+wb_position_sensor_enable(encoder_3, TIME_STEP)
+
+% initializing distance sensors
+% robot_sensor = wb_robot_get_device('robot_sensor');
+% wb_distance_sensor_enable(robot_sensor, TIME_STEP);
+
+while wb_robot_step(TIME_STEP) ~= -1
+  % reading motor encoders
+  pos_1 = wb_position_sensor_get_value(encoder_1);
+  pos_2 = wb_position_sensor_get_value(encoder_2);
+  pos_3 = wb_position_sensor_get_value(encoder_3);
+  
+  % reading distance sensors
+  % robot_presence = wb_distance_sensor_get_value(robot_sensor);
+  
+  if STAGE == 0
+    vel_1 = 0
+    vel_2 = 0
+    vel_3 = 0
+    %wb_console_print(strcat('STAGE: ', num2str(STAGE), '   Robot has not arrived. robot_presence:  ', num2str(robot_presence)), WB_STDOUT); 
+    % if robot_presence < 1000
+    if 1 < 1000
+      [theta_1,theta_2,theta_3] = find_IK(target_x,target_y,0.2,0.15)
+      vel_1 = 0
+      vel_2 = 0
+      vel_3 = 0
+      STAGE = 1
+    else
+      wb_console_print(strcat('STAGE: ', num2str(STAGE), '   Robot has not arrived. robot_presence:  ', num2str(robot_presence)), WB_STDOUT); 
+    end  
+  elseif STAGE == 1  
+    L1_con = 0
+    L2_con = 0
+    L3_con = 0
+    
+    wb_console_print(strcat('STAGE: ', num2str(STAGE), '   pos_1 : ', num2str(pos_1), '  pos_2 : ', num2str(pos_2), '  pos_3 : ', num2str(pos_3)), WB_STDOUT);
+    
+    % link 1
+    if imabsdiff( theta_1, pos_1) < 0.01
+      %wb_motor_set_velocity(motor_1, 0);
+      vel_1 = 0
+      L1_con = 1
+    else
+      if theta_1 > pos_1
+        vel_1 = 0.1
+        %wb_motor_set_velocity(motor_1, 0.1);
+      else
+        vel_1 = -0.1
+        %wb_motor_set_velocity(motor_1, -0.1);
+      end
+    end
+    
+    % link 2
+    if imabsdiff( theta_2, pos_2) < 0.01
+      %wb_motor_set_velocity(motor_2, 0);
+      vel_2 = 0
+      L2_con = 1
+    else
+      if theta_2 > pos_2
+        vel_2 = 0.1
+        %wb_motor_set_velocity(motor_2, 0.1);
+      else
+        vel_2 = -0.1
+        %wb_motor_set_velocity(motor_2, -0.1);
+      end
+    end
+    
+    % link 3
+    if imabsdiff( theta_3, pos_3) < 0.01
+      %wb_motor_set_velocity(motor_3, 0);
+      vel_3 = 0
+      L3_con = 1
+    else
+      if theta_3 > pos_3
+        vel_3 = 0.1
+        %wb_motor_set_velocity(motor_3, 0.1);
+      else
+        vel_3 = -0.1
+        %wb_motor_set_velocity(motor_3, -0.1);
+      end
+    end 
+    
+    wb_console_print(strcat('STAGE: ', num2str(STAGE), '   L1 : ', num2str(L1_con), '  L2 : ', num2str(L2_con), '  L3 : ', num2str(L3_con), '  L1*L2*L3 : ', num2str(L1_con*L2_con*L3_con)), WB_STDOUT);
+    
+    if L1_con * L2_con * L3_con == 1
+      STAGE = 2
+    end
+  elseif STAGE == 2
+    
+  end
+
+  wb_motor_set_velocity(motor_1, vel_1);
+  wb_motor_set_velocity(motor_2, vel_2);
+  wb_motor_set_velocity(motor_3, vel_3);  
+
+end
+
+% cleanup code goes here: write data to files, etc.
--- project/controllers/RRR_robot_controller/find_IK.m
+++ project/controllers/RRR_robot_controller/find_IK.m
@@ -5,12 +5,14 @@ XE = abs(X);
 YE = abs(Y+0.01);
 
 theta1 = 2*atan2((2*L_1*YE + (- L_1^4 + 2*L_1^2*L_2^2 + 2*L_1^2*XE^2 + 2*L_1^2*YE^2 - L_2^4 + 2*L_2^2*XE^2 + 2*L_2^2*YE^2 - XE^4 - 2*XE^2*YE^2 - YE^4)^(1/2)),(L_1^2 + 2*L_1*XE - L_2^2 + XE^2 + YE^2));
-theta2 = -2*atan2(((- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2)*(L_1^2 + 2*L_1*L_2 + L_2^2 - XE^2 - YE^2))^(1/2),(- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2));
-theta3 = (3*pi/2) - theta2 - theta1 - (2*pi);
+theta2 = 2*atan2(((- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2)*(L_1^2 + 2*L_1*L_2 + L_2^2 - XE^2 - YE^2))^(1/2),(- L_1^2 + 2*L_1*L_2 - L_2^2 + XE^2 + YE^2));
+
+theta3 = (0.5*pi) - theta2 + theta1;
+theta1 = (0.5*pi) - theta1
 
 if X<0
   theta1 = pi-theta1
-  theta2 = theta2
+  theta2 = -1*theta2
   theta3 = -1*theta3
 end
 
