f3ff518e948bdf90e5cb2c5208304dee525dfa56
achintha96 <achinthasurathrathnayake@gmail.com>
2024-05-13 08:55:46 +0530

    code cleaned

--- project/controllers/mobile_robot_controller/get_optimal_turn_direction.m
+++ project/controllers/mobile_robot_controller/get_optimal_turn_direction.m
@@ -1,16 +1,16 @@
 function [turn_direction] = get_optimal_turn_direction(bearing,target_bearing)
     
-    dif_accw = (bearing - target_bearing)
-    dif_ccw_cost = (360 - dif_accw)
+    dif_accw = (bearing - target_bearing);
+    dif_ccw_cost = (360 - dif_accw);
     if dif_accw<0
-        dif_accw = -dif_accw
-        dif_ccw_cost = -dif_ccw_cost
+        dif_accw = -dif_accw;
+        dif_ccw_cost = -dif_ccw_cost;
     end
     
     if dif_accw > dif_ccw_cost
-        turn_direction = 1
+        turn_direction = 1;
     else
-        turn_direction = -1
+        turn_direction = -1;
     end
 end
 
